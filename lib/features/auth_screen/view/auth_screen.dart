0097f2a19d60343ffe30cbe21a29f3b2ec142c3a
aswinmanmathans <aswinmanmathans@supportta.com>
2025-04-11 06:48:28 +0530

    Fix TODO comments for shared preferences implementation in AuthScreen

--- lib/features/auth_screen/view/auth_screen.dart
+++ lib/features/auth_screen/view/auth_screen.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 class AuthScreen extends StatelessWidget {
 
 
-//TODO : step 1 :  implement shared preference service using shaerd pref package , then go to app routes page
+//TODO : step 1 :  implement shared preference service using shaerd pref package, then go to app routes page
   static const String path = '/auth';
   const AuthScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    //TODO : step 1.1 : implement a button to nav to home page after authentication, use the shaed pref service to save weather the user is already signed in or not 
     return const Placeholder();
   }
 }
