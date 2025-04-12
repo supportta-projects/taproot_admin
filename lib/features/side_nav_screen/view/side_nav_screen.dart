e947c037c0bd31e4ad5b7a146f41c6f35535c80d
aswinmanmathans <aswinmanmathans@supportta.com>
2025-04-12 07:56:26 +0530

    Implement SideNavScreen with custom sidebar and navigation logic

--- lib/features/side_nav_screen/view/side_nav_screen.dart
+++ lib/features/side_nav_screen/view/side_nav_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:taproot_admin/constants/constants.dart';
+import 'package:taproot_admin/core/logger.dart';
+import 'package:taproot_admin/services/size_utils.dart';
+
+import '../widgets/custom_side_bar.dart';
 
 class SideNavScreen extends StatefulWidget {
   static const String path = '/sideNav';
@@ -9,13 +14,36 @@ class SideNavScreen extends StatefulWidget {
 }
 
 class _SideNavScreenState extends State<SideNavScreen> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = [
+    Container(
+      height: SizeUtils.height,
+      width: SizeUtils.width,
+      color: Colors.red,
+
+      child: Text('Home'),
+    ),
+    Center(child: Text('Settings')),
+  ];
+
+  void _onItemSelected(int index) {
+    logInfo('Tapped index: $index');
+    setState(() => _selectedIndex = index);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Side Navigation')),
-      body: Container(
-        color: Colors.green.shade100,
-        child: Center(child: Text('Side Navigation Screen')),
+      appBar: AppBar(title: const Text('Side Navigation')),
+      body: Stack(
+        children: [
+          _pages[_selectedIndex],
+          CustomSideBar(
+            selectedIndex: _selectedIndex,
+            onItemTap: _onItemSelected,
+          ),
+        ],
       ),
     );
   }
