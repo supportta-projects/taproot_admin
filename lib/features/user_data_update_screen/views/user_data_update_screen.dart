2d45f9a3128eae6516209f8b2d6567a400e25988
mohammadshahilsupportta <mohammadshahil@supportta.com>
2025-04-19 13:22:49 +0530

    feat: Implement user edit functionality with MiniGradientBorderButton for improved user experience

--- lib/features/user_data_update_screen/views/user_data_update_screen.dart
+++ lib/features/user_data_update_screen/views/user_data_update_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:lucide_icons/lucide_icons.dart';
 import 'package:taproot_admin/exporter/exporter.dart';
 import 'package:taproot_admin/features/user_data_update_screen/widgets/about_container.dart';
 import 'package:taproot_admin/features/user_data_update_screen/widgets/basic_detail_container.dart';
@@ -9,6 +10,8 @@ import 'package:taproot_admin/features/user_data_update_screen/widgets/service_c
 import 'package:taproot_admin/features/user_data_update_screen/widgets/social_container.dart';
 import 'package:taproot_admin/features/user_data_update_screen/widgets/user_profile_container.dart';
 import 'package:taproot_admin/features/users_screen/user_data_model.dart';
+import 'package:taproot_admin/widgets/mini_gradient_border.dart';
+import 'package:taproot_admin/widgets/mini_loading_button.dart';
 
 import '../widgets/additional_container.dart';
 
@@ -31,6 +34,13 @@ class _UserDataUpdateScreenState extends State<UserDataUpdateScreen> {
     super.initState();
   }
 
+  bool userEdit = false;
+  void editUser() {
+    setState(() {
+      userEdit = !userEdit;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,6 +54,88 @@ class _UserDataUpdateScreenState extends State<UserDataUpdateScreen> {
 
               child: Text('Click here to go back to the previous screen'),
             ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: CustomPadding.paddingLarge,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'User  >  User Details',
+                    style: context.inter60016.copyWith(color: Colors.green),
+                  ),
+                  Spacer(),
+                  userEdit
+                      ? MiniLoadingButton(
+                        icon: LucideIcons.save,
+                        text: 'Save',
+                        onPressed: editUser,
+                        useGradient: true,
+                        gradientColors: CustomColors.borderGradient.colors,
+                      )
+                      : MiniLoadingButton(
+                        icon: Icons.edit,
+                        text: 'Edit',
+                        onPressed: editUser,
+                        useGradient: true,
+                        gradientColors: CustomColors.borderGradient.colors,
+                      ),
+                  Gap(CustomPadding.paddingLarge.v),
+                  MiniGradientBorderButton(
+                    text: 'Back',
+                    icon: Icons.arrow_back,
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+
+                    gradient: LinearGradient(
+                      colors: CustomColors.borderGradient.colors,
+                    ),
+                  ),
+
+                  // MiniGradientBorderButton(
+                  //   text: 'Back',
+                  //   icon: Icons.arrow_back,
+                  //   onPressed: () async {
+                  //     if (userEdit) {
+                  //       final shouldDiscard = await showDialog<bool>(
+                  //         context: context,
+                  //         builder:
+                  //             (context) => AlertDialog(
+                  //               title: const Text('Discard changes?'),
+                  //               content: const Text(
+                  //                 'You have unsaved changes. Are you sure you want to go back?',
+                  //               ),
+                  //               actions: [
+                  //                 TextButton(
+                  //                   onPressed:
+                  //                       () => Navigator.of(context).pop(false),
+                  //                   child: const Text('Cancel'),
+                  //                 ),
+                  //                 TextButton(
+                  //                   onPressed:
+                  //                       () => Navigator.of(context).pop(true),
+                  //                   child: const Text('Discard'),
+                  //                 ),
+                  //               ],
+                  //             ),
+                  //       );
+                  //       if (shouldDiscard == true) {
+                  //         Navigator.pop(context);
+                  //       }
+                  //     } else {
+                  //       Navigator.pop(context);
+                  //     }
+                  //   },
+                  //   gradient: LinearGradient(
+                  //     colors: CustomColors.borderGradient.colors,
+                  //   ),
+                  // ),
+                ],
+              ),
+            ),
+            Gap(CustomPadding.paddingLarge.v),
 
             Padding(
               padding: const EdgeInsets.symmetric(
@@ -51,9 +143,9 @@ class _UserDataUpdateScreenState extends State<UserDataUpdateScreen> {
               ),
               child: Row(
                 children: [
-                  UserProfileContainer(isEdit: true),
+                  UserProfileContainer(isEdit: userEdit),
                   Gap(CustomPadding.paddingXL.v),
-                  BasicDetailContainer(user: user, isEdit: true),
+                  BasicDetailContainer(user: user, isEdit: userEdit),
                 ],
               ),
             ),
@@ -65,10 +157,10 @@ class _UserDataUpdateScreenState extends State<UserDataUpdateScreen> {
               ),
               child: Row(
                 children: [
-                  ProfileContainer(user: user, isEdit: true),
+                  ProfileContainer(user: user, isEdit: userEdit),
                   Gap(CustomPadding.paddingXL.v),
 
-                  LocationContainer(user: user, isEdit: true),
+                  LocationContainer(user: user, isEdit: userEdit),
                 ],
               ),
             ),
@@ -78,22 +170,8 @@ class _UserDataUpdateScreenState extends State<UserDataUpdateScreen> {
                 horizontal: CustomPadding.paddingLarge.v,
               ),
               child: Row(
-                children: [AdditionalContainer(user: user, isEdit: true)],
+                children: [AdditionalContainer(user: user, isEdit: userEdit)],
               ),
-            ),
-            Gap(CustomPadding.paddingXL.v),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: CustomPadding.paddingLarge.v,
-              ),
-              child: Row(children: [SocialContainer(isEdit: true, user: user)]),
-            ),
-            Gap(CustomPadding.paddingXL.v),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: CustomPadding.paddingLarge.v,
-              ),
-              child: Row(children: [AboutContainer(user: user, isEdit: true)]),
             ),
             Gap(CustomPadding.paddingXL.v),
             Padding(
@@ -101,7 +179,25 @@ class _UserDataUpdateScreenState extends State<UserDataUpdateScreen> {
                 horizontal: CustomPadding.paddingLarge.v,
               ),
               child: Row(
-                children: [ServiceContainer(isEdited: true, user: user)],
+                children: [SocialContainer(isEdit: userEdit, user: user)],
+              ),
+            ),
+            Gap(CustomPadding.paddingXL.v),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: CustomPadding.paddingLarge.v,
+              ),
+              child: Row(
+                children: [AboutContainer(user: user, isEdit: userEdit)],
+              ),
+            ),
+            Gap(CustomPadding.paddingXL.v),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: CustomPadding.paddingLarge.v,
+              ),
+              child: Row(
+                children: [ServiceContainer(isEdited: userEdit, user: user)],
               ),
             ),
             Gap(CustomPadding.paddingXXL.v),
