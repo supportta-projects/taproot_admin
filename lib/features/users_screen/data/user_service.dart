fd4b12cde8146b8372fe05a5dfadaa91b5a24ce3
mohammadshahilsupportta <mohammadshahil@supportta.com>
2025-05-01 13:38:54 +0530

    refactor: Clean up unused code in UserService; remove commented-out fetchUser method

--- lib/features/users_screen/data/user_service.dart
+++ lib/features/users_screen/data/user_service.dart
@@ -13,37 +13,9 @@ class UserService with ErrorExceptionHandler {
       );
       final data = response.data;
       return PaginatedUserResponse.fromJson(data);
-      // final userList=data['results'] as List;
-      // return userList.map((json) => User.fromJson(json)).toList();
+      
     } catch (e) {
       throw UserService().handleError(e);
     }
   }
 }
-// class UserService with ErrorExceptionHandler {
-//   static Future<PaginatedUserResponse> fetchUser({
-//     required int page,
-//     String? searchQuery,
-//     bool? showOnlyPremium,
-//   }) async {
-//     try {
-//       final queryParameters = {
-//         'page': page,
-//         if (searchQuery != null && searchQuery.isNotEmpty)
-//           'search': searchQuery,
-//         if (showOnlyPremium != null && showOnlyPremium) 'isPremium': true,
-//       };
-
-//       final response = await DioHelper().get(
-//         '/user',
-//         type: ApiType.baseUrl,
-//         queryParameters: queryParameters,
-//       );
-
-//       final data = response.data;
-//       return PaginatedUserResponse.fromJson(data);
-//     } catch (e) {
-//       throw UserService().handleError(e);
-//     }
-//   }
-// }
