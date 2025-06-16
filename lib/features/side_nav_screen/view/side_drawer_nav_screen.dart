b0c2d36573f7be300d7135f582fcee2b445af165
mohammadshahilsupportta <mohammadshahil@supportta.com>
2025-06-16 17:09:45 +0530

    refactor: Remove commented-out logout button and container from side drawer

--- lib/features/side_nav_screen/view/side_drawer_nav_screen.dart
+++ lib/features/side_nav_screen/view/side_drawer_nav_screen.dart
@@ -241,29 +241,29 @@ class _SideDrawerNavScreenState extends State<SideDrawerNavScreen> {
                     ],
                   ),
                 ),
-                Container(
-                  width: 200,
-                  color: CustomColors.secondaryColor,
-                  child: Padding(
-                    padding: EdgeInsets.all(CustomPadding.padding),
-                    child: TextButton.icon(
-                      onPressed: () => _handleLogout(context),
-                      icon: const Icon(Icons.logout, color: Colors.red),
-                      label: const Text(
-                        'Logout',
-                        style: TextStyle(
-                          color: Colors.red,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 200,
-                  height: 20,
-                  color: CustomColors.secondaryColor,
-                ),
+                // Container(
+                //   width: 200,
+                //   color: CustomColors.secondaryColor,
+                //   child: Padding(
+                //     padding: EdgeInsets.all(CustomPadding.padding),
+                //     child: TextButton.icon(
+                //       onPressed: () => _handleLogout(context),
+                //       icon: const Icon(Icons.logout, color: Colors.red),
+                //       label: const Text(
+                //         'Logout',
+                //         style: TextStyle(
+                //           color: Colors.red,
+                //           fontWeight: FontWeight.w600,
+                //         ),
+                //       ),
+                //     ),
+                //   ),
+                // ),
+                // Container(
+                //   width: 200,
+                //   height: 20,
+                //   color: CustomColors.secondaryColor,
+                // ),
               ],
             ),
           ),
