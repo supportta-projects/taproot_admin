2842cbf156c707c98fe024afd127d97b1085486f
aswinmanmathans <aswinmanmathans@suppotta.com>
2025-06-25 10:27:35 +0530

    fix: Update order screen background color for consistency

--- lib/features/order_screen/view/order_screen.dart
+++ lib/features/order_screen/view/order_screen.dart
@@ -235,7 +235,7 @@ class _OrderScreenState extends State<OrderScreen> {
                     horizontal: CustomPadding.paddingLarge.v,
                   ),
                   decoration: BoxDecoration(
-                    color: CustomColors.burgandryRed,
+                    color: CustomColors.secondaryColor,
                     borderRadius: BorderRadius.circular(
                       CustomPadding.paddingLarge.v,
                     ),
